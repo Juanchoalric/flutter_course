ec66535964e46610ba6a992e0f75f14e877faaac
unknown <juanchoalric94@gmail.com>
2018-07-24 00:53:10 -0300

    added element to control de render of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
-main() => runApp(MyApp());
-
+void main(){
+  debugPaintSizeEnabled = true;
+runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
 
   @override
